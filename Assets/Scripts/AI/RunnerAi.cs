c51ba484bc9bf18e0391190ba5f53898ca00b4e2
VeraMasc <VeraMasc@gmail.com>
2023-10-04 13:40:49 +0200

    Waypoints

--- Assets/Scripts/AI/RunnerAi.cs
+++ Assets/Scripts/AI/RunnerAi.cs
@@ -3,8 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
+/// <summary>
+/// Script encargado de gestionar a los runners
+/// </summary>
 public class RunnerAi : MonoBehaviour
 {
+	/// <summary>
+	/// Punto objetivo hacia el que ha de ir
+	/// </summary>
+	public Transform targetPoint;
+
+	/// <summary>
+	/// Punto secundario usado para suavizar el movimiento
+	/// </summary>
+	public Transform secondaryPoint;
+
+	public Vector3 effectiveTarget => targetPoint.position;
+	
+
+	/// <summary>
+	/// Índice del waypoint objetivo
+	/// </summary>
+	public int targetIndex;
+
+	//Factor de atracción del punto objetivo
+	public float targetAttraction;
 	public Vector2 velocity;
 
 	public float noise;
@@ -19,15 +42,37 @@ public class RunnerAi : MonoBehaviour
     // Update is called once per frame
     void Update()
     { 
-        if(!agent.hasPath || agent.remainingDistance <= 0.1){
-			if (agent.hasPath)
-				velocity = Quaternion.FromToRotation(Vector3.up, Vector3.forward) * agent.desiredVelocity;
-			velocity = velocity +(Random.insideUnitCircle * noise) ;
-			agent.destination =  transform.position + (Quaternion.FromToRotation(Vector3.forward, Vector3.up) * (Vector3)velocity*2);
-			agent.speed = velocity.magnitude;
-				
+        if(!agent.hasPath || agent.remainingDistance <= 1  ){
+			regeneratePath();
 		}
     }
 
-	
+	/// <summary>
+	/// Abandona el path previo (si lo hay) y genera uno nuevo
+	/// </summary>
+	public void regeneratePath(){
+		updateTargetIfNeeded();
+		// if (agent.hasPath)
+		// 		velocity = Quaternion.FromToRotation(Vector3.up, Vector3.forward) * agent.desiredVelocity;
+		// Vector2 targetDir = Quaternion.FromToRotation(Vector3.up, Vector3.forward)* (targetPoint.position - transform.position).normalized;
+		// velocity = velocity+ (targetDir * targetAttraction) +(Random.insideUnitCircle * noise) ;
+		// agent.destination =  transform.position + (Quaternion.FromToRotation(Vector3.forward, Vector3.up) * (Vector3)velocity*2);
+		// agent.speed = velocity.magnitude;
+		agent.destination = targetPoint.position;
+	}
+
+	/// <summary>
+	/// Cambia de target point si ya se ha llegado al actual
+	/// </summary>
+	public void updateTargetIfNeeded(){
+		if ((targetPoint.position - transform.position).magnitude > 2)
+			return;
+
+		(targetPoint, targetIndex) = Waypoints.singleton.getNextPoint(targetIndex,1);
+	}
+
+	void OnDrawGizmosSelected() {
+		Gizmos.color = Color.yellow;
+		Gizmos.DrawCube(effectiveTarget, Vector3.one);
+	}
 }
---
+++ Assets/Scripts/AI/Waypoints.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Singleton que controla los wayponts del recorrido de los runners
+/// </summary>
+public class Waypoints : MonoBehaviour
+{
+
+	public Transform[] waypointsList;
+
+	private static Waypoints _singleton;
+	///<summary>GameManager Singleton</summary>
+	public static Waypoints singleton
+	{
+		get 
+		{
+			if (_singleton == null)
+			{
+				_singleton = FindObjectOfType<Waypoints>(); //Para cuando el maldito hotreload me pierde la referencia
+			}
+			return _singleton;
+		}
+	}
+
+	void Awake()
+	{
+		if(singleton!=null &&  singleton != this) //Prevent duplicate singleton
+		{
+            Destroy(this);
+            return;
+        }
+
+		_singleton=this;
+	}
+    // Start is called before the first frame update
+    void Start()
+    {
+		waypointsList = getWaypoints().ToArray();
+    }
+
+	/// <summary>
+	/// Recupera una lista con todos los waypoints hijos
+	/// </summary>
+	public List<Transform> getWaypoints(){
+		var ret = new List<Transform>();
+         
+         foreach (Transform child in transform) {
+             ret.Add(child);
+
+         }
+         return ret;
+	}
+
+	/// <summary>
+	/// Obtiene el siguiente punto en el recorrido de un runner
+	/// </summary>
+	/// <param name="current">Waypoint actual del runner</param>
+	/// <param name="dir">Sentido de movimiento del runner (en la lista de waypoints)</param>
+	/// <returns>Tuple of the point's Transform & it's index</returns>
+	public (Transform, int) getNextPoint(int current, int dir){
+		var index = (current + (int)Mathf.Sign(dir)) % waypointsList.Length;
+		return (waypointsList[index], index);
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
