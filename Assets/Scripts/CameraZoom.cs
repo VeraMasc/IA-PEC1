7f1139b45607d5a6ab2422a05aa274e8f6e12ebe
VeraMasc <VeraMasc@gmail.com>
2023-10-24 22:35:45 +0200

    Camera controls

---
+++ Assets/Scripts/CameraZoom.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraZoom : MonoBehaviour
+{
+    public float minDist=5;
+	public float maxDist=10;
+
+	public CinemachineCameraOffset offset;
+
+	void Start()
+	{
+		offset = GetComponent<CinemachineCameraOffset>();
+	}
+
+
+	void Update()
+	{
+		if(Input.mouseScrollDelta.y !=0){
+			offset.m_Offset.z += Input.mouseScrollDelta.y;
+			offset.m_Offset.z = Mathf.Clamp(offset.m_Offset.z, minDist, maxDist);
+		}
+	}
+}
