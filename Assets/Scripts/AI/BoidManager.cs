52ba4e3dfd5c929b287c80e84d1eb2bfe42c1c19
VeraMasc <VeraMasc@gmail.com>
2023-11-23 14:24:08 +0100

    Calculate Nearby

--- Assets/Scripts/AI/BoidManager.cs
+++ Assets/Scripts/AI/BoidManager.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class BoidManager : MonoBehaviour
@@ -34,6 +36,7 @@ public class BoidManager : MonoBehaviour
     public float spawnRange = 6;
 
     public float neighbourDistance;
+    public float nearbyDistance;
     public float directionNoise = 0.05f;
 
     // Start is called before the first frame update
@@ -46,7 +49,9 @@ public class BoidManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-    
+        if (Input.GetKeyDown(KeyCode.C)){
+            calculateNearby();
+        }
     }
 
     void initBoids(){
@@ -57,6 +62,30 @@ public class BoidManager : MonoBehaviour
             var newBoid = (GameObject)Instantiate(boidPrefab, pos, Quaternion.LookRotation(randomize));
             allBoids[i] = newBoid.GetComponent<Boid>();
             allBoids[i].manager = this;
+        }
+    }
+
+    void calculateNearby(){
+        var nearbyDict = new Dictionary<Boid, Queue<Boid>>();
+        int num = 1;
+        foreach (Boid boid in allBoids){
+            nearbyDict.Add(boid, new Queue<Boid>());
+        }
+        foreach (Boid boid in allBoids){ //Itera todos los boids
+            for(var i = num; i<allBoids.Length; i++){ //Evita comparar con si mismo y boids ya iterados
+                float distance = Vector3.Distance(boid.transform.position, allBoids[i].transform.position);
+
+                if(distance <= nearbyDistance){
+                    nearbyDict[boid].Enqueue(allBoids[i]);
+                    nearbyDict[allBoids[i]].Enqueue(boid);
+                }
+            }
+            num++;
+        }
+
+        foreach (Boid boid in allBoids){
+            boid.boidsNearby = nearbyDict.TryGetValue(boid, out var queue)? queue.ToArray() : new Boid[0];
+            Debug.Log(queue.ToArray());
         }
     }
 
