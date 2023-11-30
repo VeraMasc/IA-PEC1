c0e07dcf10aa7964ee68a61bd9cb917956af4f5f
VeraMasc <VeraMasc@gmail.com>
2023-11-30 18:54:27 +0100

    Speech Bubble

---
+++ Assets/Scripts/AI/Behaviors/SpeakAction.cs
@@ -0,0 +1,48 @@
+using Pada1.BBCore.Tasks;
+using Pada1.BBCore;
+using UnityEngine;
+
+
+namespace BBUnity.Actions
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [Action("Speak")]
+    [Help("Dice un mensaje mediante un bocadillo")]
+    public class Speak : GOAction
+    {
+        
+
+        [InParam("speechBubble")]
+        [Help("")]
+        public GameObject speechBubble;
+
+        [InParam("duration")]
+        [Help("")]
+        public float duration;
+
+        /// <summary></summary>
+        public override void OnStart()
+        {
+        
+        }
+        /// <summary>Method of Update of MoveToRandomPosition </summary>
+        /// <remarks>Check the status of the task, if it has traveled the road or is close to the goal it is completed
+        /// and otherwise it will remain in operation.</remarks>
+        public override TaskStatus OnUpdate()
+        {
+            if (duration<=0)
+                return TaskStatus.COMPLETED;
+            duration -= Time.deltaTime;
+            return TaskStatus.RUNNING;
+        }
+
+        
+        /// <summary>Abort method of MoveToRandomPosition </summary>
+        public override void OnAbort()
+        {
+            
+        }
+    }
+}
