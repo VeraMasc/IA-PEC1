a36d43d0629f67a9f4f97924bba725e6ea8a3f22
VeraMasc <VeraMasc@gmail.com>
2023-11-30 19:14:52 +0100

    Edit Mode Improvements

--- Assets/Scripts/UI/SpeechBubble.cs
+++ Assets/Scripts/UI/SpeechBubble.cs
@@ -1,42 +1,51 @@
+using UnityEditor;
 using UnityEngine;
 
 // Basado en código de https://github.com/Firnox/Billboarding/blob/main/Billboard.cs
 
+[ExecuteAlways] //Para facilitar depuración
 public class SpeechBubble : MonoBehaviour {
-  [SerializeField] private BillboardType billboardType;
+	[SerializeField] private BillboardType billboardType;
 
-  [Header("Lock Rotation")]
-  [SerializeField] private bool lockX;
-  [SerializeField] private bool lockY;
-  [SerializeField] private bool lockZ;
+	[Header("Lock Rotation")]
+	[SerializeField] private bool lockX;
+	[SerializeField] private bool lockY;
+	[SerializeField] private bool lockZ;
 
-  private Vector3 originalRotation;
+	private Vector3 originalRotation;
 
-  public enum BillboardType { LookAtCamera, CameraForward };
+	
+	public enum BillboardType { LookAtCamera, CameraForward };
 
-  private void Awake() {
-    originalRotation = transform.rotation.eulerAngles;
-  }
+	
+	private void Awake() {
+		originalRotation = transform.rotation.eulerAngles;
+	}
 
-  // Use Late update so everything should have finished moving.
-  void LateUpdate() {
-    // There are two ways people billboard things.
-    switch (billboardType) {
-      case BillboardType.LookAtCamera:
-        transform.LookAt(Camera.main.transform.position, Vector3.up);
-        break;
-      case BillboardType.CameraForward:
-        transform.forward = Camera.main.transform.forward;
-        break;
-      default:
-        break;
-    }
-    // Modify the rotation in Euler space to lock certain dimensions.
-    Vector3 rotation = transform.rotation.eulerAngles;
-    if (lockX) { rotation.x = originalRotation.x; }
-    if (lockY) { rotation.y = originalRotation.y; }
-    if (lockZ) { rotation.z = originalRotation.z; }
-    transform.rotation = Quaternion.Euler(rotation);
+
+	
+
+	void OnRenderObject(){
+		var camera = SceneView.currentDrawingSceneView?.camera;
+		camera ??= Camera.main;
+		// There are two ways people billboard things.
+		switch (billboardType) {
+			case BillboardType.LookAtCamera:
+			transform.LookAt(camera.transform.position, Vector3.up);
+			break;
+			case BillboardType.CameraForward:
+			transform.forward = camera.transform.forward;
+			break;
+			default:
+			break;
+	}
+
+	// Modify the rotation in Euler space to lock certain dimensions.
+	Vector3 rotation = transform.rotation.eulerAngles;
+	if (lockX) { rotation.x = originalRotation.x; }
+	if (lockY) { rotation.y = originalRotation.y; }
+	if (lockZ) { rotation.z = originalRotation.z; }
+	transform.rotation = Quaternion.Euler(rotation);
   }
 }
 
