38ff7b573c5c9349d30c6dd1d140795acc189aef
VeraMasc <VeraMasc@gmail.com>
2023-12-11 21:33:53 +0100

    Circle formation

--- Assets/Scripts/AI/Formation.cs
+++ Assets/Scripts/AI/Formation.cs
@@ -22,6 +22,8 @@ public class Formation : MonoBehaviour
 
     public NavMeshAgent agent;
 
+    public FormationShape shape;
+
     public float spread = 2f;
 
     public float maxSpread = 8f;
@@ -36,5 +38,49 @@ public class Formation : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public (Vector3?, Quaternion?) getPosition(int memberNum){
+
+        if(shape == FormationShape.circle){
+            int ringNum=1, levelPos =memberNum, ringPos ;
+
+            //Encontrar en qué nivel del anillo le toca ponerse
+            while(levelPos >= ( ringPos = ringPositions(ringNum))){
+                levelPos -= ringPos;
+                ringNum++;
+            }
+            var offset =  360 * levelPos / ringPos;
+            var rotation = Quaternion.AngleAxis(offset, Vector3.up);
+            var pos = (Vector3.forward )* ringRadius(ringNum);
+            if (ringNum % 2 == 0)
+                pos *= -1; //Alternar el lado en el que empiezan los círculos
+            pos = rotation * pos;
+            return (pos, rotation);
+        }
+        return (null, null);
+    }
+
+    /// <summary>
+    /// Calcula cuantas posiciones posibles hay dentro de un nivel de la formación de anillo
+    /// </summary>
+    /// <param name="ringNum"></param>
+    /// <returns></returns>
+    public int ringPositions(int ringNum){
+        
+        return (int)(ringCircumference(ringNum) / spread);
+    }
+
+    /// <summary>
+    /// Calcula la circumferencia de un nivel de los anillos
+    /// </summary>
+    /// <param name="ringNum"></param>
+    /// <returns></returns>
+    public float ringCircumference(int ringNum){
+        return Mathf.PI * 2 * ringRadius(ringNum);
+    }
+
+    public float ringRadius(int ringNum){
+        return ringNum  * spread * 0.8f - (ringNum-1)*0.5f ;
     }
 }
--- Assets/Scripts/AI/FormationMember.cs
+++ Assets/Scripts/AI/FormationMember.cs
@@ -1,21 +1,71 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.AI;
 
+
+/// <summary>
+/// Script que gestiona a los miembros de una formación
+/// </summary>
 public class FormationMember : MonoBehaviour
 {
 
     public NavMeshAgent agent;
 
+    /// <summary>
+    /// Formación a la que pertenece
+    /// </summary>
+    public Formation formation;
+
+    public Vector3? formPosition;
+
+    public Quaternion? formRotation;
+
     // Start is called before the first frame update
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+
+        var formations = GameObject.FindObjectsOfType<Formation>();
+        var best = formations.Select(t =>
+                    new
+                    {
+                        t,
+                        distance = Vector3.Distance(t.transform.position, transform.position)
+                    }).Aggregate(new { t = (Formation)null, distance = Mathf.Infinity }, (i1, i2) => i1.distance < i2.distance ? i1 : i2);
+
+        joinFormation(best.t);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        followFormation();
+    }
+
+    public void joinFormation(Formation form){
+        var num = form.members.Count;
+        form.members.Add(this);
+        formation = form;
+        (formPosition, formRotation) = formation.getPosition(num);
+    }
+
+
+    public void followFormation(){
+        if (formation == null || formPosition == null || formRotation == null)
+            return;
+
+        var addRot = formation.transform.rotation;
+
+        if (agent.destination != formPosition + formation.transform.position){
+            agent.destination = (addRot * (Vector3)formPosition) + formation.transform.position; //actualiza la posición
+
+        } else if(!agent.pathPending && agent.remainingDistance <= agent.stoppingDistance){
+            //Girar el agente en la dirección que toca
+            transform.rotation = Quaternion.Slerp(transform.rotation, (Quaternion)formRotation * addRot, Time.deltaTime * agent.angularSpeed);
+        }
     }
 }
